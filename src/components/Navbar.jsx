231260f651f7f18ff210b3b7760b2c393e24e1e9
Ch3mson <bensonyan778@hotmail.com>
2023-12-18 18:08:39 -0500

    Added all elements, now deleting...

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,8 +44,8 @@ const Navbar = () => {
         >
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-            Benson Yan &nbsp;
-            <span className='sm:block hidden'> | Personal Portfolio</span>
+            Adrian &nbsp;
+            <span className='sm:block hidden'> | JavaScript Mastery</span>
           </p>
         </Link>
 
