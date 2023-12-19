2bf01d95daaef6ad028d9cd4c1b33bec804e75d7
Ch3mson <bensonyan778@hotmail.com>
2023-12-18 20:04:43 -0500

    Removed unecessary elements and modified text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -37,29 +37,49 @@ const ServiceCard = ({ index, title, icon }) => (
 
 const About = () => {
   return (
-    <>
-      <motion.div variants={textVariant()}>
-        <p className={styles.sectionSubText}>Introduction</p>
-        <h2 className={styles.sectionHeadText}>Overview.</h2>
-      </motion.div>
+    <div className="flex flex-wrap justify-center text-center">
 
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
-        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+        className='mt-4 text-secondary text-[17px] max-w-4xl leading-[30px]'
       >
-        I'm a skilled software developer with experience in TypeScript and
-        JavaScript, and expertise in frameworks like React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
-        create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
+        I am a student at the University of Waterloo studying <b>Computer Science</b> with a <b>double degree</b> in <b>Business Administration</b> from Wilfrid Laurier University.
       </motion.p>
 
-      <div className='mt-20 flex flex-wrap gap-10'>
-        {services.map((service, index) => (
-          <ServiceCard key={service.title} index={index} {...service} />
-        ))}
-      </div>
-    </>
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-4xl leading-[30px]'
+      >
+        I am currently in my second semester of first year, where I will be taking 6 courses: Linear Algebra 1, Calculus 2, Elementary Algorithm Design & Data Abstraction (with a lab course), Functional Areas of Organizatoins, Macroeconomics, and Interpersonal Communications
+      </motion.p>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-4xl leading-[30px]'
+      >
+        I have always been passionate about mathematics, which has led me to pursuing computer science to further challenge my logical and critical thinking abilities. 
+        Since grade 7, I have participated in math competitions, and began programming since grade 10. Step by step, I was able to find friends to mentor me into learning new languages,
+        which have eventually led me to creating this website! Currently, relatively experienced in web development, and looking to learn more about fullstack development, primarily in back-end server development. Eventually I want to branch off to different areas
+        of computer science/mathematics such as web3 development and quantitative finance.
+      </motion.p>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-4xl leading-[30px]'
+      >
+        Apart from school, I have pursued plenty of extracurricular activities. I am a competitive badminton player, having played tournaments for my high school and participated in local tournaments across British Columbia (Canada).
+        I am also a relatively competitive gamer, reaching diamond in League of Legends, and obtaining top 3000 in global ranking in osu!. I played piano since grade 1, and the trumpet since grade 6. Since then, I was
+        in my school's concert and jazz band for 7 years.
+      </motion.p>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-4xl leading-[30px]'
+      >
+        In terms of practical experience and projects, here are some of them listed below:
+      </motion.p>
+
+    </div>
   );
 };
 
